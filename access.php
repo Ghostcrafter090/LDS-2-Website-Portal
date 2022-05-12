524f2a1cefb836f409ef0ed1c87d96179b84b80d
unknown <Joshpumpkinhead@icloud.com>
2022-05-12 10:37:21 -0300

    2022-05-12 ;;; 10:37:20.65

--- access.php
+++ access.php
@@ -7,10 +7,11 @@
 	if ($minute != date("h:i")) {
 		file_put_contents(".\accessMin.cx", date("h:i"));
 		$grabbed = "yes";
-		try {
-			file_put_contents(".\current_data.json", file_get_contents("https://api.weather.com/v2/pws/observations/current?stationId=IFALLRIV2&format=json&units=s&apiKey=". $key));
-		} catch ($e) {
+		$data = @file_get_contents("https://api.weather.com/v2/pws/observations/current?stationId=IFALLRIV2&format=json&units=s&apiKey=". $key);
+		if (!($data)) {
 			file_put_contents(".\accessMin.cx", "0:0");
+		} else {
+			file_put_contents(".\current_data.json", $data);
 		}
 	}
 	$danger = file_get_contents(".\danger.txt");
