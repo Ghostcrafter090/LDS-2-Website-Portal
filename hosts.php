1eac4c788123cdd0451657bb819727c5ffe305cd
unknown <Joshpumpkinhead@icloud.com>
2022-06-05 12:27:18 -0300

    2022-06-05 ;;; 12:27:18.02

--- hosts.php
+++ hosts.php
@@ -1,18 +1,20 @@
 <?php
     $commf = false;
+    echo Date();
     if (isset($_GET['key'])) {
         if ($_GET['key'] == "30899834ufr89389ur3urueue88932") {
             if (isset($_GET['comm'])) {
                 $string = file_get_contents(".\\hosts.cxl");
-                $hosts = json_decode($string);
+                $hosts = json_decode($string, true);
                 if ($_GET['comm'] == "return") {
                     echo $string;
                     $commf = true;
-                } else if ($comm == "set") {
+                } else if ($_GET['comm'] == "set") {
                     if (isset($_GET['token'])) {
                         if (isset($_GET['value'])) {
                             $hosts[$_GET['token']] = $_GET['value'];
                             file_put_contents(".\\hosts.cxl", json_encode($hosts));
+                            echo "{\"status\": 200}";
                             $commf = true;
                         }
                     }
@@ -22,6 +24,6 @@
     }
 
     if (!$commf) {
-        echo "{\"error\": 403}";
+        echo "{\"status\": 403}";
     }
 ?>
