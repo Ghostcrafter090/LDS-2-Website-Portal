850e96c652600a3fe0ce0a4cfbd383fa67c3d687
unknown <Joshpumpkinhead@icloud.com>
2022-06-05 12:39:22 -0300

    2022-06-05 ;;; 12:39:21.73

--- hosts.php
+++ hosts.php
@@ -1,18 +1,24 @@
 <?php
     $commf = false;
-    echo Date();
     if (isset($_GET['key'])) {
         if ($_GET['key'] == "30899834ufr89389ur3urueue88932") {
             if (isset($_GET['comm'])) {
                 $string = file_get_contents(".\\hosts.cxl");
                 $hosts = json_decode($string, true);
                 if ($_GET['comm'] == "return") {
-                    echo $string;
+                    $keys = array_keys($hosts);
+                    $i = 0;
+                    while ($i < count($keys)) {
+                        if ($hosts[$keys[$i]]) {
+                            
+                        }
+                    }
                     $commf = true;
                 } else if ($_GET['comm'] == "set") {
                     if (isset($_GET['token'])) {
                         if (isset($_GET['value'])) {
-                            $hosts[$_GET['token']] = $_GET['value'];
+                            // echo "{\"ip\": ". $_GET['value']. ", \"last_updated\": ". date("Y-m-d H:i:s"). "}";
+                            $hosts[$_GET['token']] = json_decode("{\"ip\": \"". $_GET['value']. "\", \"lastUpdated\": ". $_SERVER['REQUEST_TIME']. "}", true);
                             file_put_contents(".\\hosts.cxl", json_encode($hosts));
                             echo "{\"status\": 200}";
                             $commf = true;
